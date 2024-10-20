df785ddc9e5312df08efcf8af47cafe293e8158e
TFHVoe <t.voesenek@student.fontys.nl>
2024-10-20 20:33:02 +0200

    test

--- test2.py
+++ test2.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+from urllib.error import HTTPError
 
 
 st.title('Hallo')
 st.markdown("Hallo2")
 conn = st.connection("gsheets", type = GSheetsConnection)
-df_contacts = conn.read(spreadsheet= st.secrets["gsheets"]['spreadsheet'])
+
+try:
+    df_contacts = conn.read(spreadsheet=st.secrets["gsheets"]['spreadsheet'])
+except HTTPError as e:
+    st.error(f"Failed to connect to Google Sheets: {e}")
 
