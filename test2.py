9bafb93baa912c2ce130222705b3a6b610d04872
TFHVoe <t.voesenek@student.fontys.nl>
2024-10-20 21:20:55 +0200

    test

--- test2.py
+++ test2.py
@@ -28,27 +28,3 @@ def get_gsheet_data(spreadsheet_id, worksheet_name):
     # Authorize the client
     client = gspread.authorize(credentials)
     
-    # Open the spreadsheet by ID
-    sheet = client.open_by_key(spreadsheet_id)
-    
-    # Select the worksheet
-    worksheet = sheet.worksheet(worksheet_name)
-    
-    # Get all values from the worksheet
-    data = worksheet.get_all_records()
-    
-    return pd.DataFrame(data)
-
-# Streamlit app
-st.title('Hallo')
-st.markdown("Hallo2")
-
-# Read from Google Sheets
-spreadsheet_id = st.secrets["connections"]["gsheets"]["spreadsheet_id"]
-worksheet_name = "Database-Contacts"  # Change this to your worksheet name
-
-try:
-    df_contacts = get_gsheet_data(spreadsheet_id, worksheet_name)
-    st.write(df_contacts)  # Display the data
-except Exception as e:
-    st.error(f"Error reading Google Sheets: {e}")
