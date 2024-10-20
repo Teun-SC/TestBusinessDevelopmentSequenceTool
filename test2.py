b39f58de2259c4c1db6fb3d16f42f7bdea3fcf38
TFHVoe <t.voesenek@student.fontys.nl>
2024-10-20 21:11:10 +0200

    te

--- test2.py
+++ test2.py
@@ -10,7 +10,7 @@ conn = st.connection("gsheets", type = GSheetsConnection)
 
 
 
-# df_contacts = conn.read(spreadsheet=st.secrets["gsheets"]['spreadsheet'], worksheet='Database-Contacts')
+df_contacts = conn.read(spreadsheet=st.secrets["gsheets"]['spreadsheet'], worksheet='Database-Contacts')
 # df_contacts = conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1l2UR6oq9wZTavT-wbo0229KmVJPFqwlb7bg70C6nR8E/edit?gid=0#gid=0", worksheet='Database-Contacts')
 
 
