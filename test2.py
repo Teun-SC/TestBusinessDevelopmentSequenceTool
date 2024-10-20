f6efd5a27f24206281e0558c20846796f1581e81
TFHVoe <t.voesenek@student.fontys.nl>
2024-10-20 21:19:38 +0200

    test

--- test2.py
+++ test2.py
@@ -2,7 +2,6 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
-import json
 
 # Function to get Google Sheets data
 def get_gsheet_data(spreadsheet_id, worksheet_name):
@@ -10,7 +9,18 @@ def get_gsheet_data(spreadsheet_id, worksheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     
     # Load the credentials from Streamlit secrets
-    creds = json.loads(st.secrets["connections"]["gsheets"])  # Use the TOML formatted secret
+    creds = {
+        "type": st.secrets["connections"]["gsheets"]["type"],
+        "project_id": st.secrets["connections"]["gsheets"]["project_id"],
+        "private_key_id": st.secrets["connections"]["gsheets"]["private_key_id"],
+        "private_key": st.secrets["connections"]["gsheets"]["private_key"].replace("\\n", "\n"),  # Ensure proper newline characters
+        "client_email": st.secrets["connections"]["gsheets"]["client_email"],
+        "client_id": st.secrets["connections"]["gsheets"]["client_id"],
+        "auth_uri": st.secrets["connections"]["gsheets"]["auth_uri"],
+        "token_uri": st.secrets["connections"]["gsheets"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["connections"]["gsheets"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["connections"]["gsheets"]["client_x509_cert_url"]
+    }
     
     # Create a service account credentials object
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds, scope)
